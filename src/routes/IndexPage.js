fdabd26f1620ff4d667a102c16593457fc373fe5
liushenglin <liushenglin@flybycloud.com>
2018-08-08 19:39:47 +0800

    style

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { connect } from 'dva';
-import './Home.css';
+import { Tabs } from 'antd';
+import styles from './IndexPage.css';
+
+const TabPane = Tabs.TabPane;
 
 const IndexPage = ({ loginServer, user }) => {
   return (
-    <div className="App">
-      <header className="app-header" />
-      <div className="main-content">
+    <div className={styles.app}>
+      <header className={styles.app_header} />
+      <div className={styles.main_content}>
         111
       </div>
-      <footer className="app-footer" />
+      <Tabs defaultActiveKey="1" tabPosition="bottom">
+        <TabPane tab="Tab 1" key="1">Content of Tab Pane 1</TabPane>
+        <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
+        <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
+      </Tabs>
     </div>
   );
 }
